d9919fb43ee3776e8d1dcac8f1d459d702c65189
miteshkoladiya <miteshkoladiya1117@gmail.com>
2026-02-10 17:00:01 +0530

    third change into alpha

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // Primitive types
 let appName: string = "My TypeScript App";
 let version: number = 1.0;
-let isProduction: boolean = true;
+let isProduction: boolean = false;
 
 // Arrays
 let users: string[] = ["Alice", "Bob", "Charlie"];
